4c9f5770ec4d300b0050b70ffa99c77d5173c33e
Pumuckl0 <240224977+Pumuckl0@users.noreply.github.com>
2025-12-29 01:39:32 +0100

    added comments to css and js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
+// Debug Ausgabe
 console.log("Website ready ✅");
 
-// Mobile Nav Toggle
+// Elemente aus dem HTML holen
+// document.querySelector(...) sucht ein Element im HTML
+// -> [data-nav-toggle] heißt: „Suche ein Element mit diesem data-Attribut“
 const toggleBtn = document.querySelector("[data-nav-toggle]");
 const navList = document.querySelector("[data-nav-list]");
 
+//Sicherheitscheck: Mach nur weiter, wenn beide Elemente toggleBtn und navList existieren
+
+//toggleBtn.addEventListener("click",   Wartet auf einen Klick auf den Hamburger-Button
+
+//toggle("is-open") -> variable isOpen ist true wenn Menü offen -> darauf reagiert in CSS -nav ul.is-open
+//-> Ohne is-open → display: none; Mit is-open → display: flex
+
+// Accessibility: Für Screenreader, damit weiß dass Menü offen -> toggleBtn.setAttribute("aria-expanded", String(isOpen))
 if (toggleBtn && navList) {
   toggleBtn.addEventListener("click", () => {
     const isOpen = navList.classList.toggle("is-open");
